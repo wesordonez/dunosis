bbc16f5622c20f4982813ed561e7051e7813fdfd
andresriberae <kcandres21@gmail.com>
2025-01-14 23:32:41 -0400

    subiendo las imagenes

--- styling/static_src/tailwind.config.js
+++ styling/static_src/tailwind.config.js
@@ -50,6 +50,15 @@ module.exports = {
     ],
     theme: {
         extend: {
+            animation: {
+                "loop-scroll": "loop-scroll 50s linear infinite",
+            },
+            keyframes: {
+                "loop-scroll": {
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(-100%)" },
+                },
+            },
             fontFamily: {
                 custom: ['Noir Pro', 'sans-serif'],
             },
