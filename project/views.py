f0327f7c03856ca5b8e4cefd8e8068c7e4bb66c7
Wesley Ordoñez <wesordonez1@gmail.com>
2025-01-16 17:22:03 -0600

    created landing.html and added meta data

--- project/views.py
+++ project/views.py
@@ -30,3 +30,6 @@ def home_view(request):
 
 def about_view(request):
     return render(request, 'about.html', status=200)
+
+def landing_view(request):
+    return render(request, 'landing.html', status=200)
