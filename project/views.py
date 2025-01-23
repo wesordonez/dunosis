e0b507d13240af6e80eaa256e0beb4a2ae645eaf
Andres Ribera <kcandres21@gmail.com>
2025-01-23 10:17:07 -0400

    estructura basica

--- project/views.py
+++ project/views.py
@@ -36,3 +36,6 @@ def landing_view(request):
   
 def service_details_view(request):
     return render(request, 'service-details.html', status=200)
+
+def services_view(request):
+    return render(request, 'services.html', status=200)
