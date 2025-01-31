84ccc53a0c78b2fb956db0fd65fcdabbf19186b8
Wesley Ordoñez <wesordonez1@gmail.com>
2025-01-30 20:26:48 -0600

    added and fixed js for poster hover effect on hero

--- templates/js/home.js
+++ templates/js/home.js
@@ -2,46 +2,35 @@
  * Contains JS code for home page
  */
 
+// JS for poster hover effect
 
-
-const reviewContainer = document.querySelector(".review-container")
-const reviewSlideShow = new SlideShow(reviewContainer, true, 10000)
-
-
-const reviewModal = new Modal(document.querySelector("#modal"))
-
-/** Selecciona todos los elementos con la clase 'poster' */
-const elements = document.querySelectorAll('.poster');
+const elements = document.querySelectorAll(".poster");
 
 elements.forEach(el => {
- const height = el.clientHeight;
- const width = el.clientWidth;
+	const height = el.clientHeight;
+	const width = el.clientWidth;
 
- el.addEventListener('mousemove', (e) => {
-     const { layerX, layerY } = e;
+	el.addEventListener("mousemove", e => {
+		const { layerX, layerY } = e;
 
-     const yRotation = (
-         (layerX - width / 2) / width
-     ) * 20;
+		const yRotation = ((layerX - width / 2) / width) * 20;
 
-     const xRotation = (
-         (layerY - height / 2) / height
-     ) * 20;
+		const xRotation = ((layerY - height / 2) / height) * 20;
 
-     const string = `
+		const string = `
          perspective(500px)
          scale(1.08)
          rotateX(${xRotation}deg)
          rotateY(${yRotation}deg)`;
 
-     el.style.transform = string;
- });
+		el.style.transform = string;
+	});
 
- el.addEventListener('mouseout', () => {
-     el.style.transform = `
-     perspective(500px) 
-     scale(1)
-     rotateX(0)
-     rotateY(0)`;
- });
+	el.addEventListener("mouseout", () => {
+		el.style.transform = `
+            perspective(500px) 
+            scale(1)
+            rotateX(0)
+            rotateY(0)`;
+	});
 });
