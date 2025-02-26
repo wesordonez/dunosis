17008af4d38169d3368705d2f47cdcd20e6adb45
Wesley Ordoñez <wesordonez1@gmail.com>
2025-02-25 20:47:49 -0600

    removed tw prefix from js files

--- templates/js/portfolio-details.js
+++ templates/js/portfolio-details.js
@@ -1,14 +1,14 @@
 function toggleCollapse() {
-    const content = document.getElementById('collapsibleContent');
-    const button = document.getElementById('toggleButton');
+	const content = document.getElementById("collapsibleContent");
+	const button = document.getElementById("toggleButton");
 
-    if (content.classList.contains('tw-hidden')) {
-        content.classList.remove('tw-hidden');
-        arrowIcon.classList.replace('bi-chevron-right', 'bi-chevron-down'); 
-        // button.textContent = 'Hide Project Information';
-    } else {
-        content.classList.add('tw-hidden');
-        arrowIcon.classList.replace('bi-chevron-down', 'bi-chevron-right'); 
-        // button.textContent = 'Show Project Information';
-    }
+	if (content.classList.contains("hidden")) {
+		content.classList.remove("hidden");
+		arrowIcon.classList.replace("bi-chevron-right", "bi-chevron-down");
+		// button.textContent = 'Hide Project Information';
+	} else {
+		content.classList.add("hidden");
+		arrowIcon.classList.replace("bi-chevron-down", "bi-chevron-right");
+		// button.textContent = 'Show Project Information';
+	}
 }
