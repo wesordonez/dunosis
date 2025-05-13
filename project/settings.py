7c2c4d5f9dfe8e72e38e2bc3a467158e6e2b4180
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-13 14:45:44 -0500

    installed daisy ui for admin, added blog app to nav bar again

--- project/settings.py
+++ project/settings.py
@@ -86,8 +86,10 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
-
+    
+    'django_daisy',
     'django.contrib.admin',
+    'django.contrib.humanize',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
